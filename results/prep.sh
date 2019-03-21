acdf106166e17213827fed6c500f4e236af43338
arthurdehgan <arthurdehgan@gmail.com>
2019-03-21 17:36:59 -0400

    added GRU 4.2

--- results/prep.sh
+++ results/prep.sh
@@ -1,6 +1,6 @@
-mv GRU_ADAM-18509691.out PB4_2/GRU_ADAM_log.txt
+mv GRU_ADAM-18513608.out PB4_2/GRU_ADAM_log.txt
 mv 'GRU_ADAM_model=GRU_optimizer=ADAM_initial_lr=0.0001_batch_size=20_seq_len=35_hidden_size=1500_num_layers=2_dp_keep_prob=0.35_save_best_0' PB4_2/
-mv GRU_SGD-18509693.out PB4_2/GRU_SGD.txt
+mv GRU_SGD-18513609.out PB4_2/GRU_SGD.txt
 mv 'GRU_SGD_model=GRU_optimizer=SGD_initial_lr=10_batch_size=20_seq_len=35_hidden_size=1500_num_layers=2_dp_keep_prob=0.35_save_best_0' PB4_2/
 
 mv TRANSFORMER_ADAM-18513610.out PB4_2/TRANSFORMER_ADAM_log.txt
